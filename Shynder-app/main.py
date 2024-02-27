e8f91f32ddc156f5297150e07b7382f390f1cbc8
Ivan Sen <senivan1997@gmail.com>
2024-02-27 23:58:26 +0200

    Fixed variable names in main

--- Shynder-app/main.py
+++ Shynder-app/main.py
@@ -1,8 +1,5 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse, FileResponse
-from fastapi.staticfiles import StaticFiles
-import os
-
 from sql import models, schemas, db_wrapper
 from sql.database import SessionLocal, engine
 
@@ -18,6 +15,7 @@ def get_db():
 
 @app.get("/", response_class=HTMLResponse)
 async def root():
+    db_wrapper.create_user(get_db().__next__(), schemas.UserCreate(username="test", ddescription="test", age=18, email="", ppassword="test", test_results="test"))
     html_con = ""
     with open("./static/index.html", "r", encoding="utf-8") as file:
         html_con = '\n'.join(file.readlines())
@@ -30,10 +28,10 @@ async def favicon():
 
 @app.get("/checkauth/")
 async def validate_user_login(login:str = "", password:str = ""):
-    db = SessionLocal()
+    db = get_db().__next__()
     user = db_wrapper.get_user_by_email(db, login)
     if user:
-        if user._password == password:
+        if user.ppassword == password:
             return {"message": "Success"}
         return {"message": "Incorrect password"}
     return {"message": "User not found"}

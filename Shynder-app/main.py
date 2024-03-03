70b4e43e736e8ef6279929eabca183fce79b5a9b
Ivan Sen <senivan1997@gmail.com>
2024-03-03 21:04:06 +0200

    Fix number of workers

--- Shynder-app/main.py
+++ Shynder-app/main.py
@@ -144,4 +144,4 @@ async def get_user_by_email(email:str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, workers=4)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=False)

7323f94cce9147571ac9793209036509eb324f4d
Ivan Sen <senivan1997@gmail.com>
2024-03-12 14:18:12 +0200

    changed ws connection string

--- Shynder-app/static/chats/chats.js
+++ Shynder-app/static/chats/chats.js
@@ -6,7 +6,7 @@ const menuIcon = document.querySelector(".menuIcon");
 const chat = document.querySelector(".chat");
 
 user_session_id = '1';
-var websocket = new WebSocket('wss://shynder.com.ua/chats_websocket/'+user_session_id);
+var websocket = new WebSocket('ws://20.117.195.190/chats_websocket/'+user_session_id);
 var receivers = [];
 let chat_opened = false;
 // MessageJson{

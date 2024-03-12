03f26883f01365a49571f0d394d4811e96ea8523
Ivan Sen <senivan1997@gmail.com>
2024-03-12 13:55:48 +0200

    fixed link in verify()

--- Shynder-app/main.py
+++ Shynder-app/main.py
@@ -244,7 +244,7 @@ async def verify(token:str):
         db_wrapper.create_user(db, waiting_verification[token])
         del waiting_verification[token]
         html = ""
-        with open("./static/login/login.html", "r", encoding="utf-8") as file:
+        with open("./static/login/login_page.html", "r", encoding="utf-8") as file:
             html = '\n'.join(file.readlines())
         return HTMLResponse(content=html)
     return {"message": "Token not found"}
